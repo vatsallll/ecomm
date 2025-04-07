ebcc6b72b48113640dcbd25f054a0e23932f2191
Arnav <83133851+arnav2718@users.noreply.github.com>
2025-04-07 21:26:40 +0530

    Add files via upload

---
+++ Ecomm_service-registry/src/test/java/com/example/ecomm_serviceregistry/EcommServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ecomm_serviceregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommServiceRegistryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
